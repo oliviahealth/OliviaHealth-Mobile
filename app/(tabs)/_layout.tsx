7b2ddeaa597041c57aa044010eea82a60f4c5710
PalaashPandey <palaashpandey2006@gmail.com>
2025-11-01 11:57:23 -0500

    Basic layout for tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="about-us"
+        options={{
+          title: 'About Us',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="info-circle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/(tabs)/profile.tsx to edit this screen.</Text>
     </View>
   );
 }
