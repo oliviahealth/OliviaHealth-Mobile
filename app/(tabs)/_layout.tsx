59dfc8f8bcb01aeaae2c9e74ea7345a323bb5e29
PalaashPandey <palaashpandey2006@gmail.com>
2025-11-01 12:12:52 -0500

    Switched up icons slightly

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
-import { TINT_COLOR } from '../theme';
+import { TINT_COLOR } from '../../theme';
 
 export default function TabLayout() {
   return (
@@ -9,28 +9,28 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="saved"
         options={{
           title: 'Saved',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="bookmark-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="about-us"
         options={{
           title: 'About Us',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="info-circle" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="information-circle-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="person-outline" color={color} />,
         }}
       />
     </Tabs>
