eefbb8db4fcae456b0af8e3424a607704b5fc29f
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-11-01 12:21:52 -0500

    Add splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,10 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.setOptions({
+  duration: 1500,
+  fade: false
+})
 
 export default function RootLayout() {
   return <Stack />;
