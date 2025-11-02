0e92fcb19602a5e0a8da1d3ef41811709c7ff1df
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-11-02 10:40:07 -0600

    Add OliviaHealth Branding Logo and search input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { TextInput, View, Image } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const oliviahealth_branding = require('../../assets/images/oliviahealth_branding.png')
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/(tabs)/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, paddingTop: 30, paddingHorizontal: 30 }}>
+      <View style={{ flex: 1, gap: 10 }}>
+        <Image
+          source={oliviahealth_branding}
+          style={{ width: 200, resizeMode: 'contain' }}
+        />
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: '#fafafa', borderRadius: 15, paddingHorizontal: 15, height: 45 }}>
+          <Ionicons name="search" size={20} color="#aaa" style={{ marginRight: 10 }} />
+          <TextInput
+            placeholder="Looking for something specific?"
+            placeholderTextColor="#aaa"
+            style={{ flex: 1, fontSize: 16, color: '#555' }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
