9dead3f4493d3c28a985e0da2885431e77225ef9
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-11-03 12:18:21 -0600

    fetch resources externally

---
+++ src/store/useResourcesStore.ts
@@ -0,0 +1,59 @@
+import { create } from 'zustand';
+
+export interface ILocalResources {
+    id: string,
+    title: string,
+    description: string,
+    video_url: string,
+    thumbnail_url: string,
+    spotlight?: boolean
+}
+
+export interface IVideoSpotlights {
+    id: string,
+    title: string,
+    subtitle: string,
+    video_url: string,
+    video_description: string,
+    transcript: string,
+    thumbnail_url: string,
+    spotlight?: boolean
+}
+
+export interface IQuickTips {
+    id: string,
+    title: string,
+    video_url: string,
+    infographic_url: string,
+    infographic_description: string,
+    thumbnail_url: string,
+    spotlight?: boolean
+}
+
+export interface IInfographics {
+    id: string,
+    title: string,
+    description: string,
+    thumbnail_url: string,
+    infographic_url: string,
+    spotlight?: boolean
+}
+
+export interface IResources {
+    local_resouces: ILocalResources[],
+    video_spotlights: IVideoSpotlights[],
+    quick_tips: IQuickTips[],
+    infographics: IInfographics[]
+}
+
+export interface IResourcesState {
+    resources: IResources | null
+    setResources: (resources: IResources) => void
+}
+
+const useResourcesStore = create<IResourcesState>()((set) => ({
+    resources: null,
+    setResources: (resources) => set(() => ({ resources }))
+}));
+
+export default useResourcesStore;
