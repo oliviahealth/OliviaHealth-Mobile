e46ed7e100694c23d116f302554a1f726464535b
PalaashPandey <palaashpandey2006@gmail.com>
2025-11-06 20:09:46 -0600

    header changes

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,10 +1,15 @@
+import BackButton from "@/components/BackButton";
 import { Stack } from "expo-router";
 
 export default function HomeLayout() {
+
   return (
     <Stack screenOptions={{ contentStyle: { backgroundColor: 'white' } }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="quick-tips" options={{ title: 'Quick Tips' }} />
+      <Stack.Screen 
+        name="quick-tips" 
+        options={{ title: 'Quick Tips', headerLeft: () => <BackButton /> }}
+      />
     </Stack>
   );
 }
