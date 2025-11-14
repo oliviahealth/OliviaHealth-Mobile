99d7b9ef3b996b299c5755adade75365708c0dd4
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-11-14 12:44:24 -0600

    Update video spotlights cards ui

--- components/VideoSpotlightCard.tsx
+++ components/VideoSpotlightCard.tsx
@@ -15,33 +15,21 @@ const VideoSpotlightCard: React.FC<VideoSpotlightCardProps> = ({ videoSpotlight 
   }
 
   return (
-    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff', borderRadius: 12, padding: 12, borderWidth: 1, borderColor: '#E9E9E9', gap: 2 }} activeOpacity={0.9}>
-      <Image source={{ uri: videoSpotlight.thumbnail_url }}
-        style={{
-          width: '100%',
-          aspectRatio: 16 / 9,
-          borderRadius: 12,
-          marginBottom: 8,
-          resizeMode: 'contain',
-        }} />
+    <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff', borderRadius: 12, padding: 12, borderWidth: 1, borderColor: '#E9E9E9', gap: 2 }} activeOpacity={0.9}>
+      <Image source={{ uri: videoSpotlight.thumbnail_url }} style={{ width: 50, height: 50, borderRadius: 30, marginRight: 8, alignSelf: 'center' }} />
 
-      <View style={{ flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent: 'space-between' }}>
-        {/* TODO: Replace with proper profile picture */}
-        <Image source={{ uri: videoSpotlight.thumbnail_url }} style={{ width: 38, height: 38, borderRadius: 30, marginRight: 8, alignSelf: 'center' }} />
-
-        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', marginHorizontal: 8 }}>
-          <Text style={{ fontSize: 15, fontWeight: '600', color: '#333' }} numberOfLines={2}>
-            {videoSpotlight.title}
-          </Text>
-          <Text style={{ fontSize: 12, color: '#888' }}>
-            {videoSpotlight.subtitle}
-          </Text>
-        </View>
-
-        <Pressable onPress={goToVideoSpotlight} hitSlop={8}>
-          <Ionicons name="play-circle-outline" size={32} color="#B642D3" />
-        </Pressable>
+      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', marginHorizontal: 8 }}>
+        <Text style={{ fontSize: 15, fontWeight: '600', color: '#333' }} numberOfLines={2}>
+          {videoSpotlight.title}
+        </Text>
+        <Text style={{ fontSize: 12, color: '#888' }}>
+          {videoSpotlight.subtitle}
+        </Text>
       </View>
+
+      <Pressable onPress={goToVideoSpotlight} hitSlop={8}>
+        <Ionicons name="play-circle-outline" size={32} color="#B642D3" />
+      </Pressable>
     </TouchableOpacity>
   );
 };
