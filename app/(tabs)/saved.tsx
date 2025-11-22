2192f30c3a365de01d4ab8251c24b092c3441556
PalaashPandey <palaashpandey2006@gmail.com>
2025-11-22 12:45:16 -0600

    Padding to avoid keyboard

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -13,10 +13,11 @@ import { TINT_COLOR } from "@/theme";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import React, { useMemo, useState } from "react";
 import {
+  KeyboardAvoidingView,
   ScrollView,
   Text,
   TouchableOpacity,
-  View,
+  View
 } from "react-native";
 
 enum SavedTab {
@@ -95,192 +96,194 @@ export default function Index() {
   }, [resources?.infographics, savedResources?.infographics]);
 
   return (
-    <ScrollView
-      contentContainerStyle={{
-        paddingVertical: 20,
-        paddingHorizontal: 20,
-        gap: 18,
-        flexGrow: 1,
-      }}
-      showsVerticalScrollIndicator={false}
-    >
-      <View style={{ flex: 1, gap: 18 }}>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 5, paddingTop: 12 }}>
-          <Ionicons name="bookmark" size={24} color={TINT_COLOR} />
-          <Text style={{ fontWeight: "500", fontSize: 32 }}>Saved</Text>
-        </View>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" keyboardVerticalOffset={40}>
+      <ScrollView
+        contentContainerStyle={{
+          paddingVertical: 20,
+          paddingHorizontal: 20,
+          gap: 18,
+          flexGrow: 1,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={{ flex: 1, gap: 18 }}>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 5, paddingTop: 12 }}>
+            <Ionicons name="bookmark" size={24} color={TINT_COLOR} />
+            <Text style={{ fontWeight: "500", fontSize: 32 }}>Saved</Text>
+          </View>
 
-        <SearchBar 
-          placeholder="Search in saved" 
-          value={searchQuery} 
-          onChangeText={setSearchQuery} 
-        />
+          <SearchBar 
+            placeholder="Search in saved" 
+            value={searchQuery} 
+            onChangeText={setSearchQuery} 
+          />
 
-        {/* Tabs */}
-        <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity
-            activeOpacity={0.9}
-            style={{
-              paddingHorizontal: 10,
-              height: 50,
-              borderBottomWidth: 2,
-              borderBottomColor:
-                savedTab === SavedTab.Videos ? TINT_COLOR : "transparent",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => setSavedTab(SavedTab.Videos)}
-          >
-            <Text
+          {/* Tabs */}
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity
+              activeOpacity={0.9}
               style={{
-                color:
-                  savedTab === SavedTab.Videos ? TINT_COLOR : "#888",
-                fontWeight: "500",
+                paddingHorizontal: 10,
+                height: 50,
+                borderBottomWidth: 2,
+                borderBottomColor:
+                  savedTab === SavedTab.Videos ? TINT_COLOR : "transparent",
+                justifyContent: "center",
+                alignItems: "center",
               }}
+              onPress={() => setSavedTab(SavedTab.Videos)}
             >
-              Videos
-            </Text>
-          </TouchableOpacity>
+              <Text
+                style={{
+                  color:
+                    savedTab === SavedTab.Videos ? TINT_COLOR : "#888",
+                  fontWeight: "500",
+                }}
+              >
+                Videos
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            activeOpacity={0.9}
-            style={{
-              paddingHorizontal: 10,
-              height: 50,
-              borderBottomWidth: 2,
-              borderBottomColor:
-                savedTab === SavedTab.LocalResources
-                  ? TINT_COLOR
-                  : "transparent",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => setSavedTab(SavedTab.LocalResources)}
-          >
-            <Text
+            <TouchableOpacity
+              activeOpacity={0.9}
               style={{
-                color:
+                paddingHorizontal: 10,
+                height: 50,
+                borderBottomWidth: 2,
+                borderBottomColor:
                   savedTab === SavedTab.LocalResources
                     ? TINT_COLOR
-                    : "#888",
-                fontWeight: "500",
+                    : "transparent",
+                justifyContent: "center",
+                alignItems: "center",
               }}
+              onPress={() => setSavedTab(SavedTab.LocalResources)}
             >
-              Local
-            </Text>
-          </TouchableOpacity>
+              <Text
+                style={{
+                  color:
+                    savedTab === SavedTab.LocalResources
+                      ? TINT_COLOR
+                      : "#888",
+                  fontWeight: "500",
+                }}
+              >
+                Local
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            activeOpacity={0.9}
-            style={{
-              paddingHorizontal: 10,
-              height: 50,
-              borderBottomWidth: 2,
-              borderBottomColor:
-                savedTab === SavedTab.QuickTips ? TINT_COLOR : "transparent",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => setSavedTab(SavedTab.QuickTips)}
-          >
-            <Text
+            <TouchableOpacity
+              activeOpacity={0.9}
               style={{
-                color:
-                  savedTab === SavedTab.QuickTips ? TINT_COLOR : "#888",
-                fontWeight: "500",
+                paddingHorizontal: 10,
+                height: 50,
+                borderBottomWidth: 2,
+                borderBottomColor:
+                  savedTab === SavedTab.QuickTips ? TINT_COLOR : "transparent",
+                justifyContent: "center",
+                alignItems: "center",
               }}
+              onPress={() => setSavedTab(SavedTab.QuickTips)}
             >
-              Quick Tips
-            </Text>
-          </TouchableOpacity>
+              <Text
+                style={{
+                  color:
+                    savedTab === SavedTab.QuickTips ? TINT_COLOR : "#888",
+                  fontWeight: "500",
+                }}
+              >
+                Quick Tips
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            activeOpacity={0.9}
-            style={{
-              paddingHorizontal: 10,
-              height: 50,
-              borderBottomWidth: 2,
-              borderBottomColor:
-                savedTab === SavedTab.Infographics
-                  ? TINT_COLOR
-                  : "transparent",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => setSavedTab(SavedTab.Infographics)}
-          >
-            <Text
+            <TouchableOpacity
+              activeOpacity={0.9}
               style={{
-                color:
+                paddingHorizontal: 10,
+                height: 50,
+                borderBottomWidth: 2,
+                borderBottomColor:
                   savedTab === SavedTab.Infographics
                     ? TINT_COLOR
-                    : "#888",
-                fontWeight: "500",
+                    : "transparent",
+                justifyContent: "center",
+                alignItems: "center",
               }}
+              onPress={() => setSavedTab(SavedTab.Infographics)}
             >
-              Infographics
-            </Text>
-          </TouchableOpacity>
+              <Text
+                style={{
+                  color:
+                    savedTab === SavedTab.Infographics
+                      ? TINT_COLOR
+                      : "#888",
+                  fontWeight: "500",
+                }}
+              >
+                Infographics
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Content + empty states */}
+          {savedTab === SavedTab.Videos &&
+            (savedVideoSpotlights.length > 0 ? (
+              savedVideoSpotlights.filter((videoSpotlight) => videoSpotlight.title.toLowerCase().includes(searchQuery.toLowerCase())).map((videoSpotlight) => (
+                <VideoSpotlightCard
+                  key={videoSpotlight.id}
+                  videoSpotlight={videoSpotlight}
+                />
+              ))
+            ) : (
+              <EmptyState
+                title="No saved videos yet"
+                description="Tap the bookmark icon on any video to save it here."
+              />
+            ))}
+
+          {savedTab === SavedTab.LocalResources &&
+            (savedLocalResources.length > 0 ? (
+              savedLocalResources.filter((localResource) => localResource.title.toLowerCase().includes(searchQuery.toLowerCase())).map((localResource) => (
+                <LocalResourceCard
+                  key={localResource.id}
+                  localResource={localResource}
+                />
+              ))
+            ) : (
+              <EmptyState
+                title="No saved local resources yet"
+                description="When you save local resources, they will appear here."
+              />
+            ))}
+
+          {savedTab === SavedTab.QuickTips &&
+            (savedQuickTips.length > 0 ? (
+              savedQuickTips.filter((quickTip) => quickTip.title.toLowerCase().includes(searchQuery.toLowerCase())).map((quickTip) => (
+                <QuickTipCard key={quickTip.id} quickTip={quickTip} />
+              ))
+            ) : (
+              <EmptyState
+                title="No saved quick tips yet"
+                description="Save a quick tip to find it again quickly."
+              />
+            ))}
+
+          {savedTab === SavedTab.Infographics &&
+            (savedInfographics.length > 0 ? (
+              savedInfographics.filter((infographic) => infographic.title.toLowerCase().includes(searchQuery.toLowerCase())).map((infographic) => (
+                <InfographicCard
+                  key={infographic.id}
+                  infographic={infographic}
+                />
+              ))
+            ) : (
+              <EmptyState
+                title="No saved infographics yet"
+                description="Once you save an infographic, it will show up here."
+              />
+            ))}
         </View>
-
-        {/* Content + empty states */}
-        {savedTab === SavedTab.Videos &&
-          (savedVideoSpotlights.length > 0 ? (
-            savedVideoSpotlights.filter((videoSpotlight) => videoSpotlight.title.toLowerCase().includes(searchQuery.toLowerCase())).map((videoSpotlight) => (
-              <VideoSpotlightCard
-                key={videoSpotlight.id}
-                videoSpotlight={videoSpotlight}
-              />
-            ))
-          ) : (
-            <EmptyState
-              title="No saved videos yet"
-              description="Tap the bookmark icon on any video to save it here."
-            />
-          ))}
-
-        {savedTab === SavedTab.LocalResources &&
-          (savedLocalResources.length > 0 ? (
-            savedLocalResources.filter((localResource) => localResource.title.toLowerCase().includes(searchQuery.toLowerCase())).map((localResource) => (
-              <LocalResourceCard
-                key={localResource.id}
-                localResource={localResource}
-              />
-            ))
-          ) : (
-            <EmptyState
-              title="No saved local resources yet"
-              description="When you save local resources, they will appear here."
-            />
-          ))}
-
-        {savedTab === SavedTab.QuickTips &&
-          (savedQuickTips.length > 0 ? (
-            savedQuickTips.filter((quickTip) => quickTip.title.toLowerCase().includes(searchQuery.toLowerCase())).map((quickTip) => (
-              <QuickTipCard key={quickTip.id} quickTip={quickTip} />
-            ))
-          ) : (
-            <EmptyState
-              title="No saved quick tips yet"
-              description="Save a quick tip to find it again quickly."
-            />
-          ))}
-
-        {savedTab === SavedTab.Infographics &&
-          (savedInfographics.length > 0 ? (
-            savedInfographics.filter((infographic) => infographic.title.toLowerCase().includes(searchQuery.toLowerCase())).map((infographic) => (
-              <InfographicCard
-                key={infographic.id}
-                infographic={infographic}
-              />
-            ))
-          ) : (
-            <EmptyState
-              title="No saved infographics yet"
-              description="Once you save an infographic, it will show up here."
-            />
-          ))}
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
