a78093d5bb7757f1165dbfe72d30225f83fc109d
PalaashPandey <palaashpandey2006@gmail.com>
2026-02-09 23:20:38 -0600

    journey header

--- app/(tabs)/(journey)/_layout.tsx
+++ app/(tabs)/(journey)/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function JourneyLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Journey" }} />
+    <Stack screenOptions={{ contentStyle: { backgroundColor: "white" } }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen
         name="details/[id]"
         options={{ headerTitle: "Resource Details" }}
--- app/(tabs)/(journey)/index.tsx
+++ app/(tabs)/(journey)/index.tsx
@@ -1,11 +1,40 @@
-import { useRouter } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
+import { TINT_COLOR } from "@/theme";
+import { ScrollView, Text, View } from "react-native";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 
 export default function JourneyScreen() {
-  const router = useRouter();
   return (
-    <TouchableOpacity onPress={() => router.push("/details/123")}>
-      <Text>Journey Screen</Text>
-    </TouchableOpacity>
+    <ScrollView
+      contentContainerStyle={{ paddingTop: 20, paddingHorizontal: 20, gap: 18 }}
+      showsVerticalScrollIndicator={false}
+    >
+      {/* Header */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          gap: 12,
+        }}
+      >
+        <Text style={{ fontSize: 28, fontWeight: "600", color: "#000" }}>
+          Your Journey
+        </Text>
+        <AnimatedCircularProgress
+          size={100}
+          width={10}
+          fill={75}
+          tintColor={TINT_COLOR}
+          backgroundColor="#fff"
+          lineCap="round"
+        >
+          {(fill: number) => (
+            <Text
+              style={{ fontSize: 20, fontWeight: "500" }}
+            >{`${Math.round(fill)}%`}</Text>
+          )}
+        </AnimatedCircularProgress>
+      </View>
+    </ScrollView>
   );
 }
