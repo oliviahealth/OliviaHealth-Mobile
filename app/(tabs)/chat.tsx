621ead6dc3785f6b810447bd468266e1c1878844
Sumit Nalavade <vs.nalavade2003@gmail.com>
2026-03-02 19:07:53 -0600

    Update UX

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -90,6 +90,8 @@ export default function Chat() {
 
         setConversationId(id);
         setOllieResponses(responses);
+
+        setDrawerOpen(false);
     }
 
     const handleInputSubmit = () => {
