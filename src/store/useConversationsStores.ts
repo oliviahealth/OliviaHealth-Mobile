ba721390eab5c121bafd55fdfcbc3bb0868f0bd1
Sumit Nalavade <vs.nalavade2003@gmail.com>
2026-03-02 19:06:45 -0600

    Save and restore conversation history from local storage

--- src/store/useConversationsStores.ts
+++ src/store/useConversationsStores.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { z } from 'zod';
 import { IOllieResponse, OllieResponseSchema } from '../utils/interfaces';
@@ -14,11 +15,13 @@ export type IConversation = z.infer<typeof ConversationSchema>;
 
 export interface IConversationsState {
     conversations: { [key: string]: IConversation };
+    setConversations: (conversations: { [key: string]: IConversation }) => void
     addResponse: (response: IOllieResponse) => void; // Add response to conversation. If conversation record doesn't exist, add it
 }
 
 const useConversationsStore = create<IConversationsState>()((set, get) => ({
     conversations: {},
+    setConversations: (conversations) => set(() => ({ conversations })),
     addResponse: (response) => {
         const { conversationId } = response;
 
@@ -37,18 +40,36 @@ const useConversationsStore = create<IConversationsState>()((set, get) => ({
                     responses: [],
                 } as IConversation);
 
-            return {
-                conversations: {
-                    ...state.conversations,
-                    [conversationId]: {
-                        ...conversation,
-                        date_updated: now,
-                        responses: [...conversation.responses, response],
-                    },
+
+            const updatedConversationHistory = {
+                [conversationId]: {
+                    ...conversation,
+                    date_updated: now,
+                    responses: [...conversation.responses, response],
                 },
+                ...state.conversations,
+            }
+
+            AsyncStorage.setItem('conversationHistory', JSON.stringify(updatedConversationHistory))
+                .catch(error => console.error('Error saving to AsyncStorage:', error));
+
+            return {
+                conversations: updatedConversationHistory,
             };
         });
     },
 }))
+
+export const loadSavedConversations = async () => {
+    try {
+        const savedData = await AsyncStorage.getItem('conversationHistory');
+        if (savedData) {
+            const parsedData: { [key: string]: IConversation } = JSON.parse(savedData);
+            useConversationsStore.getState().setConversations(parsedData);
+        }
+    } catch (error) {
+        console.error('Error loading from AsyncStorage:', error);
+    }
+};
 
 export default useConversationsStore;
